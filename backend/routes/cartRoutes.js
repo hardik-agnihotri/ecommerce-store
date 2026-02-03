ccacd418c61277d6d5c88ddffef5b1ab71213d4c
hardik <hardik22oct@gmail.com>
2026-02-04 02:23:24 +0530

    cart get cart and remove route add

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware";
 import { isloggedIn } from "../middlewares/loggedInMiddleware";
+import { addToCart, getCart } from "../controllers/cartController";
 
 const router = express.Router();
 
-router.post("/cart/add",protect,isloggedIn,)
+router.post("/cart/add",protect,isloggedIn,addToCart);
+router.get("/cart",protect,isloggedIn,getCart);
+router.delete("/cart/:productId",protect,isloggedIn,removeItem);
 
 
 export default router;
